ddc69a401541d9fc30902155628c9b16115afcb6
Corbin Crutchley <git@crutchcorn.dev>
2025-05-03 22:48:46 -0700

    chore: WIP

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
 	out: "./drizzle",
-	schema: "./src/db/schema/index.ts",
+	schema: "../../packages/db/src/schema/index.ts",
 	dialect: "postgresql",
 	casing: "snake_case",
 	dbCredentials: {
-		url: process.env.POSTGRES_URL,
+		url: process.env.POSTGRES_URL!,
 	},
 });
