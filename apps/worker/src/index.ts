a7725b7274d3c1b5db28addf51fb51e157e573d8
James Fenn <me@jfenn.me>
2025-10-20 23:54:02 +0000

    add createWorker call for sync-author

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -4,4 +4,5 @@ import { Tasks } from "@playfulprogramming/common";
 
 createWorker(Tasks.POST_IMAGES, "./tasks/post-images/processor.ts");
 createWorker(Tasks.URL_METADATA, "./tasks/url-metadata/processor.ts");
+createWorker(Tasks.SYNC_AUTHOR, "./tasks/sync-author/processor.ts");
 createHealthcheck();
