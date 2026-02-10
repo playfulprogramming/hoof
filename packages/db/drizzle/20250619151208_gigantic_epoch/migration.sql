4a336b117c2f31ae5111068f733502baf4640b5b
James Fenn <me@jfenn.me>
2026-02-09 20:32:13 -0500

    modify drizzle migrations to run over prod db

--- packages/db/drizzle/20250619151208_gigantic_epoch/migration.sql
+++ packages/db/drizzle/20250619151208_gigantic_epoch/migration.sql
@@ -1,10 +1,10 @@
-CREATE TABLE "post_images" (
+CREATE TABLE IF NOT EXISTS "post_images" (
 	"slug" varchar(2048) PRIMARY KEY NOT NULL,
 	"banner_key" varchar(2048) NOT NULL,
 	"link_preview_key" varchar(2048) NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE "url_metadata" (
+CREATE TABLE IF NOT EXISTS "url_metadata" (
 	"url" varchar(2048) PRIMARY KEY NOT NULL,
 	"title" varchar(2048),
 	"icon_key" varchar(2048),
--- packages/db/drizzle/20260120005415_famous_silver_samurai/migration.sql
+++ packages/db/drizzle/20260120005415_famous_silver_samurai/migration.sql
@@ -1,7 +1,8 @@
 ALTER TABLE "collection_chapters" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
 DROP TABLE "collection_chapters" CASCADE;--> statement-breakpoint
-ALTER TABLE "collection_data" ADD COLUMN "cover_image" text;--> statement-breakpoint
-ALTER TABLE "collection_data" ADD COLUMN "social_image" text;--> statement-breakpoint
-ALTER TABLE "posts" ADD COLUMN "collection_slug" text;--> statement-breakpoint
-ALTER TABLE "posts" ADD COLUMN "collection_order" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE "collection_data" ADD COLUMN IF NOT EXISTS "cover_image" text;--> statement-breakpoint
+ALTER TABLE "collection_data" ADD COLUMN IF NOT EXISTS "social_image" text;--> statement-breakpoint
+ALTER TABLE "posts" ADD COLUMN IF NOT EXISTS "collection_slug" text;--> statement-breakpoint
+ALTER TABLE "posts" ADD COLUMN IF NOT EXISTS "collection_order" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE "posts" DROP CONSTRAINT IF EXISTS "posts_collection_slug_collections_slug_fk";--> statement-breakpoint
 ALTER TABLE "posts" ADD CONSTRAINT "posts_collection_slug_collections_slug_fk" FOREIGN KEY ("collection_slug") REFERENCES "public"."collections"("slug") ON DELETE set null ON UPDATE no action;
