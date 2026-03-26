fa5d085b1e29a88f782d6a3b7bdb45de2d635457
Furkan Emin Can <78358128+femincan@users.noreply.github.com>
2026-03-27 02:16:32 +0300

    test: add fetchAsBotStream tests

--- apps/worker/src/utils/fetchAsBot.test.ts
+++ apps/worker/src/utils/fetchAsBot.test.ts
@@ -1,88 +1,216 @@
+import { Writable } from "stream";
 import { mockEndpoint } from "../../test-utils/server.ts";
-import { RobotDeniedError, fetchAsBot, resetCache } from "./fetchAsBot.ts";
+import {
+	RobotDeniedError,
+	fetchAsBot,
+	fetchAsBotStream,
+	resetCache,
+} from "./fetchAsBot.ts";
 
 beforeEach(() => resetCache());
 
-test("Should throw error when a 400 status code is returned", async () => {
-	const robotsUrl = new URL("https://example.com/robots.txt");
-	const url = new URL("https://example.com/test");
+describe("fetchAsBot", () => {
+	test("Should throw error when a 400 status code is returned", async () => {
+		const robotsUrl = new URL("https://example.com/robots.txt");
+		const url = new URL("https://example.com/test");
 
-	mockEndpoint({
-		path: robotsUrl,
-		body: "User-agent: *\nDisallow:\n",
-		headers: {
-			"content-type": "text/plain",
-		},
-	});
-	mockEndpoint({
-		path: url,
-		body: "",
-		status: 400,
+		mockEndpoint({
+			path: robotsUrl,
+			body: "User-agent: *\nDisallow:\n",
+			headers: {
+				"content-type": "text/plain",
+			},
+		});
+		mockEndpoint({
+			path: url,
+			body: "",
+			status: 400,
+		});
+
+		const response = fetchAsBot({ url, method: "GET" });
+		await expect(response).rejects.toThrow();
 	});
 
-	const response = fetchAsBot({ url, method: "GET" });
-	await expect(response).rejects.toThrow();
+	test("Should return successful data for a URL with no robots.txt", async () => {
+		const robotsUrl = new URL("https://example.com/robots.txt");
+		const url = new URL("https://example.com/test");
+
+		mockEndpoint({
+			path: robotsUrl,
+			status: 404,
+			body: "",
+		});
+		mockEndpoint({
+			path: url,
+			body: "Hello!",
+		});
+
+		const response = await fetchAsBot({ url, method: "GET" });
+		const body = await response.body.text();
+		expect(body).toBe("Hello!");
+	});
+
+	test("Should return successful data for a URL with a valid robots.txt", async () => {
+		const robotsUrl = new URL("https://example.com/robots.txt");
+		const url = new URL("https://example.com/test");
+
+		mockEndpoint({
+			path: robotsUrl,
+			body: "User-agent: *\nDisallow:\n",
+			headers: {
+				"content-type": "text/plain",
+			},
+		});
+		mockEndpoint({
+			path: url,
+			body: "Hello!",
+		});
+
+		const response = await fetchAsBot({ url, method: "GET" });
+		const body = await response.body.text();
+		expect(body).toBe("Hello!");
+	});
+
+	test("Should not return data for a URL disallowed by robots.txt", async () => {
+		const robotsUrl = new URL("https://example.com/robots.txt");
+		const url = new URL("https://example.com/test");
+
+		mockEndpoint({
+			path: robotsUrl,
+			body: "User-agent: *\nDisallow: /test\n",
+			headers: {
+				"content-type": "text/plain",
+			},
+		});
+		mockEndpoint({
+			path: url,
+			body: "Hello!",
+		});
+
+		const response = await fetchAsBot({ url, method: "GET" }).catch((e) => e);
+		expect(response).to.be.instanceOf(RobotDeniedError);
+		expect((response as Error).message).toBe(
+			"playful-programming/1.0 is disallowed from example.com!",
+		);
+	});
 });
 
-test("Should return successful data for a URL with no robots.txt", async () => {
-	const robotsUrl = new URL("https://example.com/robots.txt");
-	const url = new URL("https://example.com/test");
+describe("fetchAsBotStream", () => {
+	test("Should throw error when a 400 status code is returned", async () => {
+		const robotsUrl = new URL("https://example.com/robots.txt");
+		const url = new URL("https://example.com/test");
 
-	mockEndpoint({
-		path: robotsUrl,
-		status: 404,
-		body: "",
-	});
-	mockEndpoint({
-		path: url,
-		body: "Hello!",
-	});
+		mockEndpoint({
+			path: robotsUrl,
+			body: "User-agent: *\nDisallow:\n",
+			headers: {
+				"content-type": "text/plain",
+			},
+		});
+		mockEndpoint({
+			path: url,
+			body: "",
+			status: 400,
+		});
 
-	const response = await fetchAsBot({ url, method: "GET" });
-	const body = await response.body.text();
-	expect(body).toBe("Hello!");
-});
-
-test("Should return successful data for a URL with a valid robots.txt", async () => {
-	const robotsUrl = new URL("https://example.com/robots.txt");
-	const url = new URL("https://example.com/test");
-
-	mockEndpoint({
-		path: robotsUrl,
-		body: "User-agent: *\nDisallow:\n",
-		headers: {
-			"content-type": "text/plain",
-		},
-	});
-	mockEndpoint({
-		path: url,
-		body: "Hello!",
+		await expect(
+			fetchAsBotStream({
+				url,
+				method: "GET",
+				writable: new Writable({
+					write(_, __, next) {
+						next();
+					},
+				}),
+			}),
+		).rejects.toThrow();
 	});
 
-	const response = await fetchAsBot({ url, method: "GET" });
-	const body = await response.body.text();
-	expect(body).toBe("Hello!");
-});
+	test("Should return successful data for a URL with no robots.txt", async () => {
+		const robotsUrl = new URL("https://example.com/robots.txt");
+		const url = new URL("https://example.com/test");
 
-test("Should not return data for a URL disallowed by robots.txt", async () => {
-	const robotsUrl = new URL("https://example.com/robots.txt");
-	const url = new URL("https://example.com/test");
+		mockEndpoint({
+			path: robotsUrl,
+			status: 404,
+			body: "",
+		});
+		mockEndpoint({
+			path: url,
+			body: "Hello!",
+			repeatTimes: 2,
+		});
 
-	mockEndpoint({
-		path: robotsUrl,
-		body: "User-agent: *\nDisallow: /test\n",
-		headers: {
-			"content-type": "text/plain",
-		},
+		let body = "";
+		await fetchAsBotStream({
+			url,
+			method: "GET",
+			writable: new Writable({
+				write(chunk, _, next) {
+					body += chunk;
+					next();
+				},
+			}),
+		});
+		expect(body).toBe("Hello!");
 	});
-	mockEndpoint({
-		path: url,
-		body: "Hello!",
+
+	test("Should return successful data for a URL with a valid robots.txt", async () => {
+		const robotsUrl = new URL("https://example.com/robots.txt");
+		const url = new URL("https://example.com/test");
+
+		mockEndpoint({
+			path: robotsUrl,
+			body: "User-agent: *\nDisallow:\n",
+			headers: {
+				"content-type": "text/plain",
+			},
+		});
+		mockEndpoint({
+			path: url,
+			body: "Hello!",
+			repeatTimes: 2,
+		});
+
+		let body = "";
+		await fetchAsBotStream({
+			url,
+			method: "GET",
+			writable: new Writable({
+				write(chunk, _, next) {
+					body += chunk;
+					next();
+				},
+			}),
+		});
+		expect(body).toBe("Hello!");
 	});
 
-	const response = await fetchAsBot({ url, method: "GET" }).catch((e) => e);
-	expect(response).to.be.instanceOf(RobotDeniedError);
-	expect((response as Error).message).toBe(
-		"playful-programming/1.0 is disallowed from example.com!",
-	);
+	test("Should not return data for a URL disallowed by robots.txt", async () => {
+		const robotsUrl = new URL("https://example.com/robots.txt");
+		const url = new URL("https://example.com/test");
+
+		mockEndpoint({
+			path: robotsUrl,
+			body: "User-agent: *\nDisallow: /test\n",
+			headers: {
+				"content-type": "text/plain",
+			},
+		});
+		mockEndpoint({
+			path: url,
+			body: "Hello!",
+			repeatTimes: 2,
+		});
+
+		const error = await fetchAsBotStream({
+			url,
+			method: "GET",
+			writable: new Writable(),
+		}).catch((e) => e as Error);
+		expect(error).toBeInstanceOf(RobotDeniedError);
+		expect(error?.message).toBe(
+			`playful-programming/1.0 is disallowed from ${url.hostname}!`,
+		);
+	});
 });
