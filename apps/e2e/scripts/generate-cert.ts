81765bfee49d6cef74df56cebb9c55e33ac8acf8
fennifith <me@jfenn.me>
2026-09-21 18:11:19 -0400

    fix ci

--- apps/e2e/scripts/generate-cert.ts
+++ apps/e2e/scripts/generate-cert.ts
@@ -4,9 +4,11 @@ import path from "path";
 
 const outputDir = path.join(import.meta.dirname, "../src/generated");
 
-const attrs = [{ name: "commonName", value: "example.com" }];
-const pems = await selfsigned.generate(attrs);
+const pems = await selfsigned.generate([
+	{ name: "commonName", value: "example.com" },
+]);
 
+await fs.mkdir(outputDir, { recursive: true });
 await fs.writeFile(path.join(outputDir, "key.pem"), pems.private, "utf8");
 console.log("Private key saved to key.pem");
 
