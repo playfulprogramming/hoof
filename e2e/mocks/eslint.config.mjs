b3db8125b84f92920c59165757d0e45aacefd0c0
fennifith <me@jfenn.me>
2026-09-22 18:37:12 -0400

    make e2e eslint consistent with other packages, remove unused checks

--- e2e/mocks/eslint.config.mjs
+++ e2e/mocks/eslint.config.mjs
@@ -1,22 +1,5 @@
-import baseConfig from "../../eslint.config.mjs";
+// @ts-check
 
-export default [
-	...baseConfig,
-	{
-		files: ["**/*.json"],
-		rules: {
-			"@nx/dependency-checks": [
-				"error",
-				{
-					ignoredFiles: [
-						"{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}",
-						"{projectRoot}/vite.config.{js,ts,mjs,mts}",
-					],
-				},
-			],
-		},
-		languageOptions: {
-			parser: await import("jsonc-eslint-parser"),
-		},
-	},
-];
+import rootConfig from "../../eslint.config.mjs";
+
+export default [...rootConfig];
